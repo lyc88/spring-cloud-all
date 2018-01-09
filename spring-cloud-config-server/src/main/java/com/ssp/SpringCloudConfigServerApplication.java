43aa5ce8146ebe4a775c1054b2b1d9cf4305c4ad
sunshaoping <sunshaoping01@yxqiche.com>
2018-01-09 12:32:01 +0800

    config

--- spring-cloud-config-server/src/main/java/com/ssp/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/ssp/SpringCloudConfigServerApplication.java
@@ -1,7 +1,8 @@
-package com.ssp.springcloudconfigserver;
+package com.ssp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
